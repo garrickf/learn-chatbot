3da3b83f22c98f81a4acf3335614e3d5b8a38fbc
garrickf <gfaerr@stanford.edu>
2017-09-15 02:44:34 -0700

    add nltk.txt to replace corpora

--- src/personality.py
+++ src/personality.py
@@ -1,11 +1,11 @@
 # Sentences we'll respond with if we have no idea what the user just said
 NONE_RESPONSES = [
-    #"uh whatever",
-    #"meet me at the foosball table, bro?",
-    #"code hard bro",
-    #"want to bro down and crush code?",
-    #"I'd like to add you to my professional network on LinkedIn",
-    #"Have you closed your seed round, dog?",
+    "uh whatever",
+    "meet me at the foosball table, bro?",
+    "code hard bro",
+    "want to bro down and crush code?",
+    "I'd like to add you to my professional network on LinkedIn",
+    "Have you closed your seed round, dog?",
     "what was that",
     "say what?",
     "say that again bruh"
@@ -15,6 +15,6 @@ GREETING_RESPONSES = [
     #"'sup bro", 
     #"hey", 
     #"*nods*", 
-    #"hey you get my snap?",
-    ":3"
+    "hey you get my snap?",
+    #":3"
 ]
